360c7c086630b1a25abb5e2e32750241f100d1d2
Andrey Semykin <103148680+prog20210@users.noreply.github.com>
2023-07-18 16:21:09 +0400

    Program finished and checked

--- ConsoleApp10/Program.cs
+++ ConsoleApp10/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp10
     {
         static void Main(string[] args)
         {
+            int l = 0;
+            int i = 0;
+            int elt;
+            int len;
+            int s = 0;
+            int[] mass = { 1, 2, 3, 4, 5, 6,2 };
+            List<int> num = new List<int>();
+            Console.WriteLine("Введите значение искомого элемента");
+
+            elt = Convert.ToInt32(Console.ReadLine());
+
+            len = mass.Length;
+            for (i = 0; i <= len-1; i++)
+            {
+                if (elt == mass[i]) { Console.WriteLine("Элемент найден");  Console.WriteLine(mass[i].ToString() + " " + i.ToString()); s = s + 1; }
+
+            }
+            if (s == 0) { Console.WriteLine("Элемент не найден"); }
+            Console.ReadKey();
         }
     }
+
 }
+
